6c4bf296bdf583c6bf1c8bfc87d9cd4f37ca2294
root <root@instance-1.us-central1-a.c.otus-microservices.internal>
2020-05-13 15:18:43 +0000

    fix gke-se-install.sh

--- infra/gke-se-install.sh
+++ infra/gke-se-install.sh
@@ -30,10 +30,10 @@ gcloud dns record-sets list --zone=se-project-zone | grep test.$DOMAIN || gcloud
 gcloud dns record-sets list --zone=se-project-zone | grep monitoring.$DOMAIN || gcloud dns record-sets transaction add $(kubectl get services | grep 'nginx-ingress-controller' | awk '{print $4}') --name=monitoring.$DOMAIN. --ttl=300 --type=A --zone=se-project-zone
 gcloud dns record-sets transaction execute --zone=se-project-zone
 
-sed -i 's/example.com/$DOMAIN/g' values-prometheus.yml
+sed -i "s/example.com/$DOMAIN/g" values-prometheus.yml
 helm upgrade --install prometheus  -f values-prometheus.yml stable/prometheus
 
-sed -i 's/example.com/$DOMAIN/g' values-grafana.yml
+sed -i "s/example.com/$DOMAIN/g" values-grafana.yml
 helm upgrade --install grafana  -f values-grafana.yml stable/grafana
 
 
