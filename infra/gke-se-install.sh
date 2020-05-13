832b882654dab74f5ad669d89c2b7a2d0e272c14
root <root@instance-1.us-central1-a.c.otus-microservices.internal>
2020-05-13 16:25:02 +0000

    fix gke-se-install.sh

--- infra/gke-se-install.sh
+++ infra/gke-se-install.sh
@@ -30,11 +30,27 @@ gcloud dns record-sets list --zone=se-project-zone | grep test.$DOMAIN || gcloud
 gcloud dns record-sets list --zone=se-project-zone | grep monitoring.$DOMAIN || gcloud dns record-sets transaction add $(kubectl get services | grep 'nginx-ingress-controller' | awk '{print $4}') --name=monitoring.$DOMAIN. --ttl=300 --type=A --zone=se-project-zone
 gcloud dns record-sets transaction execute --zone=se-project-zone
 
-sed -i "s/example.com/$DOMAIN/g" values-prometheus.yml
-helm upgrade --install prometheus  -f values-prometheus.yml stable/prometheus
+echo "alertmanager:
+  ingress:
+    hosts:
+      - alertmanager.$DOMAIN
 
-sed -i "s/example.com/$DOMAIN/g" values-grafana.yml
-helm upgrade --install grafana  -f values-grafana.yml stable/grafana
+server:
+  ingress:
+    hosts:
+      - monitoring.$DOMAIN
+" > values-prometheus-ingress.yml
+
+#sed -i "s/example.com/$DOMAIN/g" values-prometheus.yml
+helm upgrade --install prometheus -f values-prometheus-ingress.yml  -f values-prometheus.yml stable/prometheus
+
+echo "ingress:
+  hosts:
+    - monitoring.$DOMAIN
+" > values-grafana-ingress.yml
+
+#sed -i "s/example.com/$DOMAIN/g" values-grafana.yml
+helm upgrade --install grafana -f values-grafana-ingress.yml  -f values-grafana.yml stable/grafana
 
 
 
